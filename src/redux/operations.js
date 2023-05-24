a77a1130caad2e38baf78fae38764987af0396cd
Mykola V <98426734+Nicolai2022@users.noreply.github.com>
2023-05-24 17:47:29 +0300

    addFixBag

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -19,12 +19,12 @@ export const fetchContacts = createAsyncThunk(
 export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
-    try {
-      const response = await axios.post('/contacts', contact);
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
+    // try {
+    //   const response = await axios.post('/contacts', contact);
+    //   return response.data;
+    // } catch (e) {
+    //   return thunkAPI.rejectWithValue(e.message);
+    // }
   }
 );
 
